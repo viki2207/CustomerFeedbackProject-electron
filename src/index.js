c135431bc0aaa0898f68b3315d663db6db196cf2
viki2207 <vidhishah2207@gmail.com>
2024-04-15 13:08:23 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// let worried_smily = document.getElementById("worried_smily");
+// // worried_smily.addEventListener("click", ({send()})=>{
+// // function send(params) {
+    
+// // }
+// // console.log("Hello smily") 
+// //  
+    
+// // })
+
+
+// worried_smily.addEventListener('click', () => { send() });
+
+// function send(){
+//     console.log('ping');
+//      win.setIgnoreMouseEvents(true);
+// }
+// app.on('ready', function(){
+//     mainWindow = new BrowserWindow({width:800, height:600});
+//     mainWindow.webContents.openDevTools();
+// });
